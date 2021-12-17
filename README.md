e60296237a39a551f1230f3ca815580768574101
yezhengSTAT <yezhengSTAT@gmail.com>
2021-12-17 15:22:44 -0800

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Convert G4 hunter output from the website into BED files in the genomic coordinates
+
+This repository include the script and demodata to convert G4hunter output results into bed file in the genomic coordinate.
+
+There are a few parameters that require input file locations and tune input/output file format.
+
+```
+g4_outfile: Path to the output files from G4 hunter
+region_file: Path to the region file, can be your peak calling results or the gene region file. First column is chromosoeom, second column is region starting site, third column is region ending site. The rest columns will not be used so can be nothing or anything.
+separate_by: The separator of region file, can be "\t" or " " or ","
+first_position_ignore: TRUE or FALSE to indicate whether the starting position in the region starting site is included in your sequence (fasta) file. TRUE for not included, FALSE for included.
+out_file: Path to store the transformed output in BED format.
+output_include_origin_peak: TRUE or FALSE to indicate whether you want to save the original region information. FALSE to only keep the motif information.
+```
+
+Demon runs: In terminal or in a shell script.
+
+```
+g4_outfile="....../demoData/demo_g4hunter_output.csv"
+region_file <- "....../demoData/AllgenesChrStartEndNameStrand.csv"
+separate_by <- ","
+first_position_ignore <- FALSE
+out_file <- "....../path/to/the/output/folder/demo_g4huter_motif.bed"
+output_include_origin_peak <- FALSE
+
+Rscript convert_G4hunter_out_to_bedfiles.R "${g4_outfile}" "${region_file}" "${separate_by}" "${first_position_ignore}" "${out_file}" "${output_include_origin_peak}"
+
+```
---
+++ scripts/convert_G4hunter_out_to_bedfiles.R
@@ -0,0 +1,61 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+g4_outfile <- args[1]
+peak_file <- args[2] ## col1 chrom, col2 start, col3 end
+separate_by <- args[3]
+first_position_ignore <- args[4]
+output_include_origin_peak <- args[5]
+
+# g4_outfile <- "/fh/fast/gottardo_r/yezheng_working/G-quadruplex/results/CSV/dm6_g4_grouped_window10_thres1.5.csv"
+# region_file <- "/fh/fast/gottardo_r/yezheng_working/G-quadruplex/data/AllgenesChrStartEndNameStrand.csv"
+# separate_by <- ","
+# first_position_ignore <- FALSE
+# out_file <- "/fh/fast/gottardo_r/yezheng_working/G-quadruplex/results/CSV/dm6_g4_grouped_window10_thres1.5_motif.bed"
+# output_include_origin_peak <- FALSE
+
+g4_out <- read.table(g4_outfile, header = T, sep = "\t")
+region <- read.table(region_file, sep = separate_by, header = F, fill = T)
+
+if(first_position_ignore){
+    region$width <- region$V3 - region$V2
+}else{
+    region$width <- region$V3 - region$V2 + 1
+}
+
+get_g4_hunter_motif <- function(g4_res, region) {
+    region_seq_len <- cumsum(data.frame(region)$width)
+    motif_coord <- c()
+    ind <- 1
+    for (i in 1:nrow(g4_res)) {
+        while (g4_res$POSITION[i] > region_seq_len[ind]) {
+            ind <- ind + 1
+        }
+        if(ind > 1){
+            position <- g4_res$POSITION[i] - region_seq_len[ind - 1]
+        }else{
+            position <- g4_res$POSITION[i]
+        }
+
+        motif_coord <- rbind(motif_coord, data.frame(
+            chrom = region[ind, ]$V1,
+            region_start = region[ind, ]$V2,
+            region_end = region[ind, ]$V3,
+            motif_start = region[ind, ]$V2 + position,
+            motif_end = region[ind, ]$V2 + position + g4_res$LENGTH[i] - 1,
+            motif_length = g4_res$LENGTH[i],
+            motif_score = g4_res$SCORE[i],
+            motif_sequence = g4_res$SEQUENCE[i],
+            motif_strand = gsub("1", "+", gsub("-1", "-", sign(g4_res$SCORE[i])))
+        ))
+    }
+    return(motif_coord)
+}
+res <- get_g4_hunter_motif(g4_out, region)
+
+if(output_include_origin_peak){
+    write.table(res, file = out_file, quote = F, col.names = T, row.names = F, sep = "\t")
+}else{
+
+    write.table(res[, c("chrom", "motif_start", "motif_end", "motif_length", "motif_score", "motif_sequence", "motif_strand")], file = out_file, quote = F, col.names = T, row.names = F, sep = "\t")
+}
